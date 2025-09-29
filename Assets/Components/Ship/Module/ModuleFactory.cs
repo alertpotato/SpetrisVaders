8adbd72f3a82ae6645ac9d7e12c81c4af3763c05
alertpotato <mousedemon@yandex.ru>
2025-09-29 22:01:48 +0300

    Basic module outfit randomization

--- Assets/Components/Ship/Module/ModuleFactory.cs
+++ Assets/Components/Ship/Module/ModuleFactory.cs
@@ -1,9 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 public class ModuleFactory : MonoBehaviour
 {
     public static ModuleFactory Instance;
-
+    public Dictionary<int, int> outfitNumberWeignts;
     public ShipModuleData[] allModules;
 
     public GameObject modulePrefab;
@@ -11,6 +13,13 @@ public class ModuleFactory : MonoBehaviour
     void Awake()
     {
         Instance = this;
+        outfitNumberWeignts = new Dictionary<int, int>
+        {
+            { 1, 1000 },
+            { 2, 150 },
+            { 3, 15 },
+            { 4, 1 }
+        };
     }
 
     public GameObject GetModule(string moduleName = null, Transform parent = null)
@@ -21,10 +30,16 @@ public class ModuleFactory : MonoBehaviour
             Debug.LogError("No module found: " + moduleName);
             return null;
         }
-        var actualData = new ShipModuleStats(data);
+        // Outfit randomization
+        int[] keys = outfitNumberWeignts.Keys.ToArray();
+        int[] weights = outfitNumberWeignts.Values.ToArray();
 
-        //actualData.shape[i] = new CellData(actualData.shape[i].localPosition,OutfitType.Canon);
-
+        int index = WeightFunctions.GetRandomWeightedIndex(weights);
+        int outfitsNumber = keys[index];
+        var outfitPositions = OutfitRandomizer(outfitsNumber);
+        // Data class
+        var actualData = new ShipModuleStats(data, outfitPositions);
+        
         GameObject obj = Instantiate(modulePrefab, Vector3.zero, Quaternion.identity, parent);
         ShipModule moduleS = obj.GetComponent<ShipModule>();
         moduleS.Initialize(actualData);
@@ -52,7 +67,7 @@ public class ModuleFactory : MonoBehaviour
             Debug.LogWarning("No modules assigned to factory!");
             return null;
         }
-
+        
         var randomData = allModules[Random.Range(0, allModules.Length)];
 
         Vector3 pos = Camera.main.ViewportToWorldPoint(
@@ -63,5 +78,22 @@ public class ModuleFactory : MonoBehaviour
         obj.transform.SetParent(transform);
         return obj;
     }
+    public static int[] OutfitRandomizer(int count)
+    {
+        count = Mathf.Clamp(count, 0, 4);
+        List<int> all = new List<int> { 0, 1, 2, 3 };
+        List<int> chosen = new List<int>();
 
+        for (int i = 0; i < count; i++)
+        {
+            int idx = Random.Range(0, all.Count);
+            chosen.Add(all[idx]);
+            all.RemoveAt(idx);
+        }
+
+        return chosen.ToArray();
+    }
 }
+
+
+    
--- Assets/Components/Ship/Module/ShipModuleSO.cs
+++ Assets/Components/Ship/Module/ShipModuleSO.cs
@@ -1,15 +1,33 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 [CreateAssetMenu(fileName = "ShipModuleData", menuName = "Game/Ship Module Data")]
 public class ShipModuleData : ScriptableObject
 {
     public string moduleName;
     public ModuleType type;
+    public OutfitType outfitType;
     public CellData[] shape;
     public int baseHealth = 4;
     public Sprite mainSprite;
     public Sprite backSprite;
     public Sprite outfitSprite;
+}
+[CreateAssetMenu(fileName = "ShipModuleData", menuName = "Game/Ship Modules Weights")]
+public class ShipModuleWeights : ScriptableObject
+{
+    public string weightName;
+    public List<ModuleWeights> weights;
+}
+[System.Serializable]
+public struct ModuleWeights
+{
+    [Tooltip("Name")]public string name;
+    [Tooltip("Weight")]public int weight;
+    public ModuleWeights( string moduleName,int moduleWeight)
+    {
+        name = moduleName;weight=moduleWeight;
+    }
 }
 
 [System.Serializable]
@@ -29,20 +47,35 @@ public class ShipModuleStats
 {
     public string moduleName;
     public ModuleType type;
+    public OutfitType outfitType;
     public CellData[] shape;
     public int baseHealth;
     public Sprite mainSprite;
     public Sprite backSprite;
     public Sprite outfitSprite;
     
-    public ShipModuleStats(ShipModuleData data)
+    public ShipModuleStats(ShipModuleData data, int[] outfitPositions=null)
     {
         moduleName = data.moduleName;
         type = data.type;
+        outfitType = data.outfitType;
         shape = (CellData[])data.shape.Clone();
         baseHealth = data.baseHealth;
         mainSprite = data.mainSprite;
         backSprite = data.backSprite;
         outfitSprite = data.outfitSprite;
+        
+        ApplyOutfits(outfitPositions,data.outfitType);
+    }
+    public void ApplyOutfits(int[] indices, OutfitType type)
+    {
+        if (indices == null || indices.Length == 0) return;
+        foreach (int idx in indices)
+        {
+            if (idx >= 0 && idx < shape.Length)
+            {
+                shape[idx] = new CellData(shape[idx].localPosition, type);
+            }
+        }
     }
 }
